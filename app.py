6cafcee51daf2f802977e6bb7015ff347ecc2bfc
Dylan Shepard <dylan@shepard.dev>
2020-06-12 21:09:05 -0700

    working us-west-2

--- app.py
+++ app.py
@@ -2,17 +2,27 @@
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 # coding: utf-8
 
+import boto3
 from flask import Flask, request
 
 app = Flask(__name__)
 
 
-@app.route('/<var>')
-def yay(var):
-    return var, 200
+@app.route('/')
+def yay():
+    return 'yay!', 200
 
 
-@app.route('/game', methods=['POST'])
+@app.route('/game', methods=['POST', 'GET'])
 def gamestartup():
-    data = request.json
-    return data, 200
+    if request.method == 'POST':
+        data = request.json
+        return data, 200
+    else:
+        return 'game', 200
+
+
+@app.route('/aaron/<mood>')
+def aaron(mood):
+    string = 'an {} aaron is watching me right now!'.format(mood)
+    return string, 200
